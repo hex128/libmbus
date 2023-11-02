f9ad3fbc22c600bd929b5e0ac8e1bf5eaf712605
Andrew Shulgin <andrewshulginua@gmail.com>
2023-11-02 19:09:05 +0200

    mbus-{serial,tcp}-request-data-csv: fixed volume value for VIF 0x14

--- bin/mbus-serial-request-data-csv.c
+++ bin/mbus-serial-request-data-csv.c
@@ -209,9 +209,13 @@ main(int argc, char **argv) {
                         serial = mbus_data_bcd_decode_hex(record->data, record->data_len);
                         break;
                     case 0x13:
-                    case 0x14:
                         if (record->drh.dib.dif == 0x04 || record->drh.dib.dif == 0x0C) {
                             volume = strtod(mbus_data_record_value(record), NULL) / 1000;
+                        }
+                        break;
+                    case 0x14:
+                        if (record->drh.dib.dif == 0x04 || record->drh.dib.dif == 0x0C) {
+                            volume = strtod(mbus_data_record_value(record), NULL) / 100;
                         }
                         break;
                     case 0xFD:
